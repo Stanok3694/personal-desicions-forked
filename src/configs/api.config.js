89d6fa67dc05a86a27cf73833559c0d6764780b7
Stanislav Okunev <stanok3694@yandex.ru>
2018-02-28 02:30:11 +0300

    deploy: fixed URL for api calls

--- src/configs/api.config.js
+++ src/configs/api.config.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     dev: "http://localhost:3001/",
-    prod: "https://personal-decisions-api.herokuapp.com/"
+    prod: "https://personal-desicion-api.herokuapp.com/"
 }
 
 module.exports = apiConfig;
