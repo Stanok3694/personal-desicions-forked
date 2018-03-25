550ad4bb113b7277067980cc9116ceef0a409fb8
Stanislav Okunev <stanok3694@yandex.ru>
2018-03-26 00:51:10 +0300

    deploy: switched to real env + tested

--- src/configs/api.config.js
+++ src/configs/api.config.js
@@ -2,8 +2,8 @@ const apiConfig = {
     dev: "http://localhost:3001/",
     prod: "https://personal-desicion-api.herokuapp.com/",
     // SO: current env
-    whichEnv: "dev",
-    // whichEnv: "prod",
+    // whichEnv: "dev",
+    whichEnv: "prod",
 }
 
 const apiConfigSwitcher = () => {
